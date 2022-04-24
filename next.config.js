16f745874c933ec9fb10b4a233fee0d948f0550a
Kim Myungseong <forwarm5891@gmail.com>
2022-04-25 05:08:20 +0900

    fix: fix next.config.js and env file

--- next.config.js
+++ next.config.js
@@ -4,25 +4,34 @@ const API_KEY = process.env.API_KEY;
 
 module.exports = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+    secondSecret: process.env.API_KEY, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/static',
+  },
   async redirects() {
     return [
       {
-        source: "/contact/:path*",
-        destination: "/new-contact/:path*",
+        source: '/contact/:path*',
+        destination: '/new-contact/:path*',
         permanent: false,
-      }
-    ]
+      },
+    ];
   },
   async rewrites() {
     return [
       {
-        source: "/api/movies",
-        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko&page=1`
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko&page=1`,
       },
       {
-        source: "/api/movies/:id",
-        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}&language=ko&page=1`
-      }
-    ]
-  }
-}
+        source: '/api/movies/:id',
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}&language=ko&page=1`,
+      },
+    ];
+  },
+};
